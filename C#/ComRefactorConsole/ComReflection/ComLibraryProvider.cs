dd90b29ef2bd287ed6da84726beb7b95dde6e0d9
Mark Johnstone <MarkJohnstoneGitHub@users.noreply.github.com>
2023-06-03 02:05:16 +1000

    Added ComRectorConsole
    Testing to obtain TypeInfo for a type library.
    
    Command line argument is the path  to the type library eg.  H:\Users\Me\source\repos\DotNetLib\bin\release\DotNetLib.tlb
    
    Output eg.
    <comClass clsid="{72E3AB6F-9742-4F2F-8FA2-43FEEB1CC788}" tlbid="{65E9265F-0806-492D-8E05-72DBFF7D85C6}" description="Represents an instant in time, typically expressed as a date and time of day." progid="H:\Users\Me\source\repos\DotNetLib\bin\release\DotNetLib.tlb.DateTime"></comClass>
    
    <comClass clsid="{27660912-6101-4779-B4E0-24F2B164B334}" tlbid="{65E9265F-0806-492D-8E05-72DBFF7D85C6}" description="Represents a point in time, typically expressed as a date and time of day, relative to Coordinated Universal Time (UTC)." progid="H:\Users\Me\source\repos\DotNetLib\bin\release\DotNetLib.tlb.DateTimeOffset"></comClass>
    
    <comClass clsid="{B73DFD69-6C69-4CFC-89F2-1C344228A9D4}" tlbid="{65E9265F-0806-492D-8E05-72DBFF7D85C6}" description="Represents a time interval." progid="H:\Users\Me\source\repos\DotNetLib\bin\release\DotNetLib.tlb.TimeSpan"></comClass>
    
    <comClass clsid="{A27D393F-5F4D-4F9B-9A5C-A72D980C802A}" tlbid="{65E9265F-0806-492D-8E05-72DBFF7D85C6}" description="Represents any time zone in the world." progid="H:\Users\Me\source\repos\DotNetLib\bin\release\DotNetLib.tlb.TimeZoneInfo"></comClass>
    
    <comClass clsid="{C88C9749-4D9C-46D0-A463-5DA93F0E1A75}" tlbid="{65E9265F-0806-492D-8E05-72DBFF7D85C6}" description="Represents a strongly typed list of objects that can be accessed by index. Provides methods to search, sort, and manipulate lists." progid="H:\Users\Me\source\repos\DotNetLib\bin\release\DotNetLib.tlb.List"></comClass>
    
    <comClass clsid="{70EF1DD1-80FA-4276-8051-426C0FAFB2FA}" tlbid="{65E9265F-0806-492D-8E05-72DBFF7D85C6}" description="Represents a strongly typed list of strings that can be accessed by index. Provides methods to search, sort, and manipulate lists." progid="H:\Users\Me\source\repos\DotNetLib\bin\release\DotNetLib.tlb.ListString"></comClass>
    
    <comClass clsid="{E4643BBE-74AF-467D-8DF3-06B681E93E4C}" tlbid="{65E9265F-0806-492D-8E05-72DBFF7D85C6}" description="Provides the base class for a generic read-only collection." progid="H:\Users\Me\source\repos\DotNetLib\bin\release\DotNetLib.tlb.ReadOnlyCollection"></comClass>

---
+++ C#/ComRefactorConsole/ComReflection/ComLibraryProvider.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+using Rubberduck.Parsing.ComReflection;
+
+namespace ComRefactorConsole.ComReflection
+{
+
+    // https://github.com/rubberduck-vba/Rubberduck/blob/next/Rubberduck.Parsing/ComReflection/ComLibraryProvider.cs
+    // https://learn.microsoft.com/en-us/windows/win32/api/oaidl/nn-oaidl-itypelib
+
+
+    public class ComLibraryProvider
+    {
+
+        #region Native Stuff
+        /// <summary>
+        /// Controls how a type library is registered.
+        /// </summary>
+        private enum REGKIND
+        {
+            /// <summary>
+            /// Use default register behavior.
+            /// </summary>
+            REGKIND_DEFAULT = 0,
+            /// <summary>
+            /// Register this type library.
+            /// </summary>
+            REGKIND_REGISTER = 1,
+            /// <summary>
+            /// Do not register this type library.
+            /// </summary>
+            REGKIND_NONE = 2
+        }
+
+        [DllImport("oleaut32.dll", CharSet = CharSet.Unicode)]
+        private static extern int LoadTypeLibEx(string strTypeLibName, REGKIND regKind, out ITypeLib TypeLib);
+        #endregion
+
+        public static ITypeLib LoadTypeLibrary(string libraryPath)
+        {
+            LoadTypeLibEx(libraryPath, REGKIND.REGKIND_NONE, out var typeLibrary);
+            return typeLibrary;
+        }
+
+        public static IComDocumentation GetComDocumentation(ITypeLib typelib)
+        {
+            try
+            {
+                return new ComDocumentation(typelib, ComDocumentation.LibraryIndex);
+            }
+            catch
+            {
+                return null;
+            }
+        }
+
+        //public ReferenceInfo GetReferenceInfo(ITypeLib typelib, string name, string path)
+        //{
+        //    try
+        //    {
+        //        typelib.GetLibAttr(out var attributes);
+        //        using (DisposalActionContainer.Create(attributes, typelib.ReleaseTLibAttr))
+        //        {
+        //            var typeAttr = Marshal.PtrToStructure<System.Runtime.InteropServices.ComTypes.TYPELIBATTR>(attributes);
+
+        //            return new ReferenceInfo(typeAttr.guid, name, path, typeAttr.wMajorVerNum, typeAttr.wMinorVerNum);
+        //        }
+        //    }
+        //    catch
+        //    {
+        //        return ReferenceInfo.Empty;
+        //    }
+        //}
+    }
+}
---
+++ C#/ComRefactorConsole/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Runtime.InteropServices.ComTypes;
+using System.Text;
+using System.Threading.Tasks;
+using ComRefactorConsole.ComReflection;
+
+namespace ComRefactorConsole
+{
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = args[0];
+            //H:\Users\Me\source\repos\DotNetLib\bin\release\DotNetLib.tlb
+
+            //string path = @"H:\Users\Me\source\repos\VBA\VBA COM Refractor\DotNetLib.tlb";
+            if (File.Exists(path))
+            {
+
+
+                ITypeLib typeLib = ComLibraryProvider.LoadTypeLibrary(path);
+
+
+                // https://stackoverflow.com/questions/43875454/is-there-a-way-to-view-com-entries-by-traversing-a-tlb-file-in-net
+                int count = typeLib.GetTypeInfoCount();
+                IntPtr ipLibAtt = IntPtr.Zero;
+                typeLib.GetLibAttr(out ipLibAtt);
+
+                var typeLibAttr = (System.Runtime.InteropServices.ComTypes.TYPELIBATTR)
+                    Marshal.PtrToStructure(ipLibAtt, typeof(System.Runtime.InteropServices.ComTypes.TYPELIBATTR));
+                Guid tlbId = typeLibAttr.guid;
+
+
+                for (int i = 0; i < count; i++)
+                {
+                    ITypeInfo typeInfo = null;
+                    typeLib.GetTypeInfo(i, out typeInfo);
+
+                    //figure out what guids, typekind, and names of the thing we're dealing with
+                    IntPtr ipTypeAttr = IntPtr.Zero;
+                    typeInfo.GetTypeAttr(out ipTypeAttr);
+
+                    //unmarshal the pointer into a structure into something we can read
+                    var typeattr = (System.Runtime.InteropServices.ComTypes.TYPEATTR)
+                        Marshal.PtrToStructure(ipTypeAttr, typeof(System.Runtime.InteropServices.ComTypes.TYPEATTR));
+
+                    System.Runtime.InteropServices.ComTypes.TYPEKIND typeKind = typeattr.typekind;
+                    Guid typeId = typeattr.guid;
+
+                    //get the name of the type
+                    string strName, strDocString, strHelpFile;
+                    int dwHelpContext;
+                    typeLib.GetDocumentation(i, out strName, out strDocString, out dwHelpContext, out strHelpFile);
+
+
+                    if (typeKind == System.Runtime.InteropServices.ComTypes.TYPEKIND.TKIND_COCLASS)
+                    {
+                        string xmlComClassFormat = "<comClass clsid=\"{0}\" tlbid=\"{1}\" description=\"{2}\" progid=\"{3}.{4}\"></comClass>";
+                        string comClassXml = String.Format(xmlComClassFormat,
+                            typeId.ToString("B").ToUpper(),
+                            tlbId.ToString("B").ToUpper(),
+                            strDocString,
+                            path, strName
+                            );
+                        Debug.WriteLine(comClassXml);
+                        Console.WriteLine(comClassXml);
+                    }
+                    else if (typeKind == System.Runtime.InteropServices.ComTypes.TYPEKIND.TKIND_INTERFACE)
+                    {
+                        string xmlProxyStubFormat = "<comInterfaceExternalProxyStub name=\"{0}\" iid=\"{1}\" tlbid=\"{2}\" proxyStubClsid32=\"{3}\"></comInterfaceExternalProxyStub>";
+                        string proxyStubXml = String.Format(xmlProxyStubFormat,
+                            strName,
+                            typeId.ToString("B").ToUpper(),
+                            tlbId.ToString("B").ToUpper(),
+                            "{00020424-0000-0000-C000-000000000046}"
+                        );
+                        Debug.WriteLine(proxyStubXml);
+                        Console.WriteLine(proxyStubXml);
+                    }
+
+                }
+
+                return;
+            }
+        }
+    }
+}
